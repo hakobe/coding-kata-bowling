7a577a6a60b29ed19f9da5a2babd85acc7f71ce1
hakobe <hakobe@gmail.com>
2014-04-20 02:11:45 +0900

    more readable

--- scala5/src/main/scala/Game.scala
+++ scala5/src/main/scala/Game.scala
@@ -6,13 +6,13 @@ class Game(rolls: Iterable[Int]) {
   private[this] def _score(rs: List[Int], s: Int) : Int = {
     rs match {
       // 10フレーム目
-      case first :: second :: third :: Nil if first == 10 || first + second == 10 =>
+      case first :: second :: third :: Nil if isStrike(first) || isSpare(first, second) =>
         s + first + second + third
       // スペア
-      case first :: second :: next :: other if first + second == 10 =>
+      case first :: second :: next :: other if isSpare(first, second) =>
         _score(next :: other, s + 10 + next)
       // ストライク
-      case first :: next :: secondNext :: other if first == 10 =>
+      case first :: next :: secondNext :: other if isStrike(first) =>
         _score(next :: secondNext :: other, s + 10 + next + secondNext)
       // 通常フレーム
       case first :: second :: other =>
@@ -21,6 +21,9 @@ class Game(rolls: Iterable[Int]) {
         s
     }
   }
+
+  private[this] def isStrike(pins: Int) : Boolean = pins == 10
+  private[this] def isSpare(pinsFirst: Int, pinsSecond: Int) : Boolean = pinsFirst + pinsSecond == 10
 
   def score() : Int =  _score(rolls.toList, 0)
 }
